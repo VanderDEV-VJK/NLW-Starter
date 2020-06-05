55193fa18376814ce75469370dc02ab352bf8492
VanderDEV-VJK <dev.vjk@gmail.com>
2020-06-04 21:50:34 -0300

    Início do Front-End projeto Ecoleta

---
+++ scripts/intro.js
@@ -0,0 +1,69 @@
+// coments
+//document.write("Hello")
+
+// variáveis, tipos de dados
+
+//let myvar = "Hello"
+//const myconst = "World"
+//document.write(myvar +" "+ myconst)
+
+// string
+// const s1 = "Isso é uma String"
+// const s2 = 'Isso é uma String'
+// const s3 = `Isso é uma String`
+
+// number
+// const n1 = 10
+// const n2 = 2
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: false,
+//  correr(){
+//    return "run Forest"
+//  }
+// }
+
+// document.write(pessoa.correr())
+
+
+// Array - vetores - Coleção de dados
+// const colecaoDeBolinhas = ["blue", "green", 1 {name:"My name"}]
+
+// document.write(colecaoDeBolinhas[2].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name) {
+
+//   document.write(name)
+// }
+
+// executar
+// sayMyName("Douglas")
+// sayMyName("Vander")
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+// i++ é igual a i+i
+
+// for (i = 0; i < 10; i++) {
+//  document.write(`<p>${i}</p>`)
+// }
+
